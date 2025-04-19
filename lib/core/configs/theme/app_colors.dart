c6271f77ccc929d2ac1a82f1b454cad936679e01
Shahad Haider <139572455+shalsha01@users.noreply.github.com>
2025-04-19 21:13:57 +0300

    Add app_colors.dart for theme color configuration

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class Appcolors{
+  static const Color primaryColor = Color(0xFF42C83C);
+  static const Color lightBackgroundColor = Color(0xFFF2F2F2);
+  static const Color darkBackgroundColor = Color(0xFF0D0C0C);
+  static const Color grayColor = Color(0xFFBEBEBE);
+  static const Color darkGrayColor = Color(0xFF343434);
+
+}
