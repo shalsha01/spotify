66d5e3dbc592b438dd1920c4047957c8bffe00cf
Shahad Haider <139572455+shalsha01@users.noreply.github.com>
2025-04-26 19:47:11 +0300

    Add path_provider and hydrated_bloc dependencies, update Podfiles, and modify main function for initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:spotify/core/configs/theme/app_theme.dart';
 import 'package:spotify/presentation/splash/pages/splash.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorageDirectory.web
+        : HydratedStorageDirectory((await getTemporaryDirectory()).path),
+  );
+  runApp( const MyApp());
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
