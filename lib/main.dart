d712fe1e9a67c5f2480b33734e7eb0f45e8ddb4e
Shahad Haider <139572455+shalsha01@users.noreply.github.com>
2025-04-26 20:20:47 +0300

    Integrate Hydrated Bloc for theme management and update main app structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:spotify/core/configs/theme/app_theme.dart';
+import 'package:spotify/presentation/choose_mode/bloc/theme_cubit.dart';
 import 'package:spotify/presentation/splash/pages/splash.dart';
 
 Future<void> main() async {
@@ -19,10 +21,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: AppTheme.lightTheme,
-      debugShowCheckedModeBanner:false,
-      home:const SplashPage(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => ThemeCubit())
+      ],
+      child: BlocBuilder<ThemeCubit,ThemeMode>(
+        builder: (context,mode) => MaterialApp(
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          themeMode: mode,
+          debugShowCheckedModeBanner: false,
+          home: const SplashPage()
+        ),
+      ),
     );
   }
     // Initialize services like Firebase, Hive, etc.
